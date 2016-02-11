6122e99deb7e671cbece06e5b1adc39e93cea891
ShreyaMittapalli <shreya.shinie@gmail.com>
2016-02-10 21:39:30 -0500

    Pushing application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Scorpion::Application.config.secret_token = 'a46fb19b2c61550f649c02e6707e3aaf5766b9c671b771d70cc2f83ebe155243dc0db7427d80c92d7eb2c92e3fedac743829c31f48645607780f8e66f64c76e2'
